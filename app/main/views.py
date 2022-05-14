454c9123a349133e2e43264d650f44646f37c8fb
Charlotte Natasha <natashaonyango97@gmail.com>
2022-05-14 21:42:20 +0200

    added card to blog.html

--- app/main/views.py
+++ app/main/views.py
@@ -1,14 +1,22 @@
 from . import main_blueprint
 from flask import render_template
 
-@main_blueprint.route('/')
+@main_blueprint.route('/home')
 def index():
     return render_template('index.html')
 
-@main_blueprint.route('/blog')
-def blog():
+@main_blueprint.route('/blogs')
+def blogs():
     return render_template('blog.html') 
 
 @main_blueprint.route('/about')
-def navbar():
+def about():
     return render_template('about.html')       
+
+@main_blueprint.route('/quotes')
+def quotes():
+    return render_template('quotes.html') 
+
+# @main_blueprint.route('/about')
+# def navbar():
+#     return render_template('about.html')         
