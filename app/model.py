64b72ccce80b7a9ab7c97c37d3bd33e302faf119
Charlotte Natasha <natashaonyango97@gmail.com>
2022-05-15 19:57:34 +0200

    added 3 new tables to the models

--- app/model.py
+++ app/model.py
@@ -39,8 +39,8 @@ class Blogs(db.Model,UserMixin):
     title=db.Column(db.String(255),nullable=False)
     post=db.Column(db.Text())
     comments=db.relationship('Comment',backref='comments',lazy='dynamic')
-    upvote=db.relationship('Upvote',backref='pitch',lazy='dynamic')
-    downvote=db.relationship('Downvote',backref='pitch',lazy='dynamic')
+    upvote=db.relationship('Upvote',backref='blogs',lazy='dynamic')
+    downvote=db.relationship('Downvote',backref='blogs',lazy='dynamic')
     user_id=db.Column(db.Integer,db.ForeignKey('users.id'))
     image_file=db.Column(db.String(255),nullable=False,default='default.jpg')
     # date_created=db.Column(db.DateTime,default=datetime.utcnow)
@@ -51,4 +51,60 @@ class Blogs(db.Model,UserMixin):
 
     def __repr__(self):
         return f'Blogs{self.post}'  
-            
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+
+    id=db.Column(db.Integer,primary_key=True)
+    comment=db.Column(db.Text(),nullable=False)
+    user_id=db.Column(db.Integer,db.ForeignKey('users.id'))
+    pitch_id=db.Column(db.Integer,db.ForeignKey('blogs.id'),nullable=False)
+
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod 
+    def get_comments(cls,id):
+        comments=Comment.query.filter_by(id=id).all()
+        return comments   
+
+    def __repr__(self):
+        return f'Blog{self.comment}'       
+
+class Upvote(db.Model):
+    __tablename__='upvotes'
+
+    id=db.Column(db.Integer,primary_key=True)
+    user_id=db.Column(db.Integer,db.ForeignKey('users.id'))
+    pitch_id=db.Column(db.Integer,db.ForeignKey('blogs.id'))
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_upvotes(cls,id):
+        downvote=Upvote.query.filter_by(blog_id=id).all()
+        return upvote    
+
+    def __repr__(self):
+        return f'{self.user_id}:{self.blog_id}' 
+class Downvote(db.Model):
+    __tablename__='downvotes'
+
+    id=db.Column(db.Integer,primary_key=True)
+    user_id=db.Column(db.Integer,db.ForeignKey('users.id'))
+    pitch_id=db.Column(db.Integer,db.ForeignKey('blogs.id'))
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_downvotes(cls,id):
+        downvote=Downvote.query.filter_by(blog_id=id).all()
+        return downvote    
+
+    def __repr__(self):
+        return f'{self.user_id}:{self.blog_id}'  
